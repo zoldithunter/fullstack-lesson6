958cf47609fa9c3923ea4b91937f480b102b5e9e
Duy Hunter <zold.ithunter@gmail.com>
2018-11-29 01:32:40 +0700

    update
    add lesson

--- js/lesson.js
+++ js/lesson.js
@@ -1,34 +1,69 @@
-var gl_Time = null;
+var t_process_bar = null;
+var t_circle = null;
 var width = 0;
 var cont = 1;
+var cont_circle = 1;
 
+// Process bar
 function frame() {
-  var elem = document.getElementById("myBar");
-  var text = document.getElementById("text_process");
-  if (width >= 100) {
-    clearInterval(gl_Time);
-  } else {
-    width++;
-    elem.style.width = width + '%';
-    text.innerHTML = width + '%';
-  }
+    var elem = document.getElementById("myBar");
+    var text = document.getElementById("text_process");
+    if (width >= 100) {
+        clearInterval(t_process_bar);
+    } else {
+        width++;
+        elem.style.width = width + '%';
+        text.innerHTML = width + '%';
+    }
 }
 
 function reset() {
-  width = 0;
-  clearInterval(gl_Time);
-  gl_Time = setInterval(frame, 100);
+    width = 0;
+    clearInterval(t_process_bar);
+    t_process_bar = setInterval(frame, 100);
 }
 
 function resume() {
-  var btn = document.getElementById("btn_Continue");
-  if (cont) {
-    clearInterval(gl_Time);
-    btn.innerHTML = "Continue";
-    cont = 0;
-  } else {
-    gl_Time = setInterval(frame, 100);
-    btn.innerHTML = "Stop";
-    cont = 1;
-  }
+    var btn = document.getElementById("btn_Procees");
+    if (cont) {
+        clearInterval(t_process_bar);
+        btn.innerHTML = "Continue";
+        cont = 0;
+    } else {
+        t_process_bar = setInterval(frame, 100);
+        btn.innerHTML = "Stop";
+        cont = 1;
+    }
+}
+
+// Circle
+function run_circle() {
+    var w = 30;
+    var h = 30;
+    var tmp = -2;
+    var circle = document.getElementById("circle");
+    t_circle = setInterval(change_circle, 100);
+
+    function change_circle () {
+        if ((w > 150 && h > 150) || (w < 30 && h < 30)) {
+            tmp = -tmp;
+        }
+        w += tmp;
+        h += tmp;
+        circle.style.width = w + 'px';
+        circle.style.height= h + 'px';
+    }
+}
+
+function stop_circle() {
+    var btn = document.getElementById("btn_Circle");
+    if (cont_circle) {
+        clearInterval(t_circle);
+        btn.innerHTML = "Continue";
+        cont_circle = 0;
+    } else {
+        run_circle();
+        btn.innerHTML = "Stop";
+        cont_circle = 1;
+    }
 }
